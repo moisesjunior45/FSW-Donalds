5b135aa304ab84019bfde6b46a079836a3ea405e
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2025-03-06 16:58:12 -0300

    feat: add initial restaurant page and menu page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      { hostname: "u9a6wmr3as.ufs.sh" },
+      { hostname: "media.gettyimages.com" },
+    ],
+  },
 };
 
 export default nextConfig;
